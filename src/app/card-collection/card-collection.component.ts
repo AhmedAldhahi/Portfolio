a3a155bf46d00fef823be0fc2b38d1d9115102d0
AhmedAldhahi <ahmed.aldhahi.a@gmail.com>
2024-10-12 17:21:09 +0300

    1.0

---
+++ src/app/card-collection/card-collection.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-collection',
+  templateUrl: './card-collection.component.html',
+  styleUrl: './card-collection.component.css'
+})
+export class CardCollectionComponent {
+
+}
---
+++ src/app/controls/controls.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-controls',
+  templateUrl: './controls.component.html',
+  styleUrl: './controls.component.css'
+})
+export class ControlsComponent {
+  toggleMode() {
+    console.log('ciel is bad at cssing');
+    console.log('ciel is bad at ');
+    throw new Error('Method not implemented.');
+
+}
+
+}
